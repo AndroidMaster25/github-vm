ac38504e82835b2d3d8019a8b46d078858f3428c
AndroidMaster25 <183199911+AndroidMaster25@users.noreply.github.com>
2026-01-06 17:08:38 +0100

    Update start.sh

--- mac/start.sh
+++ mac/start.sh
@@ -21,9 +21,8 @@ sudo defaults write /Library/Preferences/com.apple.RemoteManagement ScreenSharin
 sudo defaults write /Library/Preferences/com.apple.RemoteManagement ARD_AllLocalUsers -bool TRUE
 
 # Enable VNC
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
-    -activate -configure -allowAccessFor -allUsers -privs -all \
-    -configure -clientopts -setvnclegacy -vnclegacy -users yes
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -allowAccessFor -allUsers -privs -all
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
 
 echo runnerrdp | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
