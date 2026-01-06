cf64840cfc2689eae7d3f166c11c1bc367d82d59
AndroidMaster25 <183199911+AndroidMaster25@users.noreply.github.com>
2026-01-06 16:54:33 +0100

    Update start.sh

--- mac/start.sh
+++ mac/start.sh
@@ -1,10 +1,10 @@
-#Downloads
+# Downloads
 curl -s -o login.sh -L "https://raw.githubusercontent.com/JohnnyNetsec/github-vm/main/mac/login.sh"
 
-#disable spotlight indexing
+# Disable spotlight indexing
 sudo mdutil -i off -a
 
-#Create new account
+# Create new account
 sudo dscl . -create /Users/runneradmin
 sudo dscl . -create /Users/runneradmin UserShell /bin/bash
 sudo dscl . -create /Users/runneradmin RealName Runner_Admin
@@ -16,23 +16,24 @@ sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
 sudo createhomedir -c -u runneradmin > /dev/null
 sudo dscl . -append /Groups/admin GroupMembership runneradmin
 
-#Enable VNC
+# Enable Screen Sharing
 sudo defaults write /Library/Preferences/com.apple.RemoteManagement ScreenSharingAllowed -bool YES
 sudo defaults write /Library/Preferences/com.apple.RemoteManagement ARD_AllLocalUsers -bool TRUE
 
+# Enable VNC
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
     -activate -configure -allowAccessFor -allUsers -privs -all \
     -configure -clientopts -setvnclegacy -vnclegacy -users yes
 
 echo runnerrdp | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
-#Start VNC/reset changes
+# Start VNC/reset changes
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
-#install ngrok
+# Install ngrok
 brew install --cask ngrok
 
-#configure ngrok and start it
+# Configure ngrok and start it
 ngrok authtoken $1
 ngrok tcp 5900 --region=in &
