9f2e55fad002523424711c9c9c2b1a1c95d56220
AndroidMaster25 <183199911+AndroidMaster25@users.noreply.github.com>
2026-01-06 16:50:02 +0100

    Update start.sh

--- mac/start.sh
+++ mac/start.sh
@@ -13,16 +13,16 @@ sudo dscl . -create /Users/runneradmin PrimaryGroupID 80
 sudo dscl . -create /Users/runneradmin NFSHomeDirectory /Users/tcv
 sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
 sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
-sudo createhomedir -c -u runneradmin > /dev/null
+sudo createhomedir -c -u runneradmin > /dev/nullhttps://github.com/AndroidMaster25/github-vm/blob/main/mac/start.sh
 sudo dscl . -append /Groups/admin GroupMembership runneradmin
 
 #Enable VNC
+sudo defaults write /Library/Preferences/com.apple.RemoteManagement ScreenSharingAllowed -bool YES
+sudo defaults write /Library/Preferences/com.apple.RemoteManagement ARD_AllLocalUsers -bool TRUE
+
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
     -activate -configure -allowAccessFor -allUsers -privs -all \
     -configure -clientopts -setvnclegacy -vnclegacy -users yes
-
-sudo defaults write /Library/Preferences/com.apple.RemoteManagement ScreenSharingAllowed -bool YES
-sudo defaults write /Library/Preferences/com.apple.RemoteManagement ARD_AllLocalUsers -bool TRUE
 
 echo runnerrdp | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
